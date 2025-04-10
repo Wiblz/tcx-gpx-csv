ea7c7762d16f77e6c5015523e0cad435c657ecaf
Wiblz <wiblzcode@gmail.com>
2025-04-10 19:28:03 +0200

    Fix formatting in tcx2csv.py

--- tcx2csv.py
+++ tcx2csv.py
@@ -1,71 +1,74 @@
-import re
 import argparse
+import re
 import xml.etree.ElementTree as et
 
+
 # takes in a TCX file and outputs a CSV file
-def main (input, output):
+def main(input, output):
     tree = et.parse(input)
     root = tree.getroot()
-    m=re.match(r'^({.*})', root.tag)
+    m = re.match(r'^({.*})', root.tag)
     if m:
-        ns=m.group(1)
+        ns = m.group(1)
     else:
-        ns=''
-    if root.tag!=ns+'TrainingCenterDatabase':
-        print('Unknown root found: '+root.tag)
+        ns = ''
+    if root.tag != ns + 'TrainingCenterDatabase':
+        print('Unknown root found: ' + root.tag)
         return
-    activities=root.find(ns+'Activities')
+    activities = root.find(ns + 'Activities')
     if not activities:
         print('Unable to find Activities under root')
         return
-    activity=activities.find(ns+'Activity')
+    activity = activities.find(ns + 'Activity')
     if not activity:
         print('Unable to find Activity under Activities')
         return
-    columnsEstablished=False
-    for lap in activity.iter(ns+'Lap'):
+    columnsEstablished = False
+    for lap in activity.iter(ns + 'Lap'):
         if columnsEstablished:
             fout.write('New Lap\n')
-        for track in lap.iter(ns+'Track'):
-            #pdb.set_trace()
+        for track in lap.iter(ns + 'Track'):
+            # pdb.set_trace()
             if columnsEstablished:
                 fout.write('New Track\n')
-            for trackpoint in track.iter(ns+'Trackpoint'):
+            for trackpoint in track.iter(ns + 'Trackpoint'):
                 try:
-                    time=trackpoint.find(ns+'Time').text.strip()
+                    time = trackpoint.find(ns + 'Time').text.strip()
                 except:
-                    time=''
+                    time = ''
                 try:
-                    latitude=trackpoint.find(ns+'Position').find(ns+'LatitudeDegrees').text.strip()
+                    latitude = trackpoint.find(ns + 'Position').find(ns + 'LatitudeDegrees').text.strip()
                 except:
-                    latitude=''
+                    latitude = ''
                 try:
-                    longitude=trackpoint.find(ns+'Position').find(ns+'LongitudeDegrees').text.strip()
+                    longitude = trackpoint.find(ns + 'Position').find(ns + 'LongitudeDegrees').text.strip()
                 except:
-                    longitude=''
+                    longitude = ''
                 try:
-                    altitude=trackpoint.find(ns+'AltitudeMeters').text.strip()
+                    altitude = trackpoint.find(ns + 'AltitudeMeters').text.strip()
                 except:
-                    altitude=''
+                    altitude = ''
                 try:
-                    bpm=trackpoint.find(ns+'HeartRateBpm').find(ns+'Value').text.strip()
+                    bpm = trackpoint.find(ns + 'HeartRateBpm').find(ns + 'Value').text.strip()
                 except:
-                    bpm=''
+                    bpm = ''
                 if not columnsEstablished:
-                    fout=open(output, 'w')
-                    fout.write(','.join(('Time','LatitudeDegrees','LongitudeDegrees','AltitudeMeters','heartratebpm/value'))+'\n')
-                    columnsEstablished=True
-                fout.write(','.join((time,latitude,longitude,altitude,bpm))+'\n')
+                    fout = open(output, 'w')
+                    fout.write(','.join(
+                        ('Time', 'LatitudeDegrees', 'LongitudeDegrees', 'AltitudeMeters', 'heartratebpm/value')) + '\n')
+                    columnsEstablished = True
+                fout.write(','.join((time, latitude, longitude, altitude, bpm)) + '\n')
 
     fout.close()
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     # arguments
     parser = argparse.ArgumentParser(description=
-        'something.')
+                                     'something.')
     parser.add_argument('input', help='input TCX file')
     parser.add_argument('output', help='output CSV file')
-    #parser.add_argument('--verbose', help='increase output verbosity', action='store_true')
+    # parser.add_argument('--verbose', help='increase output verbosity', action='store_true')
     args = parser.parse_args()
-    
+
     main(args.input, args.output)
